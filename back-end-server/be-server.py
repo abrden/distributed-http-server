7b30ce01126241f831708cb9f4ad981d9731a0e4
Agustina Barbetta <agustina.barbetta@gmail.com>
2018-09-15 22:47:48 -0300

    Move connection handler

--- back-end-server/be-server.py
+++ back-end-server/be-server.py
@@ -1,41 +1,11 @@
-import sys
+import sys, os
 import signal
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-import os
 sys.path.append(os.path.abspath(os.path.join('..', 'http-server')))
-from HTTPServer import HTTPServer, HTTPResponseMaker
-from sockets import ClientSocket
-
-
-def handle_client_connection(conn, address):
-    logging.basicConfig(level=logging.DEBUG)
-    logger = logging.getLogger("Worker-%r" % (address,))
-
-    try:
-        conn = ClientSocket(conn)
-
-        logger.debug("Connected %r at %r", conn, address)
-
-        data = conn.receive(1024)  # receive data from client
-        logger.debug("Received data %r", data)
-
-        string = bytes.decode(data)
-
-        response_headers = HTTPResponseMaker.response(404)
-        response_content = b"<html><body><p>Error 404: File not found</p><p>Python HTTP server</p></body></html>"
-
-        server_response = response_headers.encode()  # return headers for GET and HEAD
-        server_response += response_content  # return additional conten for GET only
-
-        conn.send(server_response)
-        logger.debug("Sent data %r", server_response)
-    except:
-        logger.exception("Problem handling request")
-    finally:
-        logger.debug("Closing connection with client")
-        conn.close()
+from httpserver import HTTPServer
+from connection_handler import handle_client_connection
 
 
 def main():
---
+++ back-end-server/connection_handler.py
@@ -0,0 +1,36 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+import sys, os
+sys.path.append(os.path.abspath(os.path.join('..', 'http-server')))
+from httpserver import HTTPResponseMaker
+from sockets import ClientSocket
+
+
+def handle_client_connection(conn, address):
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger("Worker-%r" % (address,))
+
+    try:
+        conn = ClientSocket(conn)
+
+        logger.debug("Connected %r at %r", conn, address)
+
+        data = conn.receive(1024)  # receive data from client
+        logger.debug("Received data %r", data)
+
+        string = bytes.decode(data)
+
+        response_headers = HTTPResponseMaker.response(404)
+        response_content = b"<html><body><p>Error 404: File not found</p><p>Python HTTP server</p></body></html>"
+
+        server_response = response_headers.encode()  # return headers for GET and HEAD
+        server_response += response_content  # return additional conten for GET only
+
+        conn.send(server_response)
+        logger.debug("Sent data %r", server_response)
+    except:
+        logger.exception("Problem handling request")
+    finally:
+        logger.debug("Closing connection with client")
+        conn.close()
