c6dfbf4be9f1ea8c0608f618f287d97b0978f81f
Agustina Barbetta <agustina.barbetta@gmail.com>
2018-09-23 18:33:19 -0300

    Final env variables

--- front_end_server/__main__.py
+++ front_end_server/__main__.py
@@ -9,7 +9,7 @@ def main():
     logger = logging.getLogger("FE-Server")
     logger.info("Starting FE Server")
 
-    s = FrontEndServer(os.environ['FE_HOST'], int(os.environ['HTTP_SERVER_PORT']), os.environ['FE_HOST'], int(os.environ['BRIDGE_PORT']), int(os.environ['BE_NUM']))
+    s = FrontEndServer(os.environ['HOST'], int(os.environ['HTTP_SERVER_PORT']), os.environ['FE_HOST'], int(os.environ['BRIDGE_PORT']), int(os.environ['BE_NUM']))
     s.start()
 
     logger.info("Done")
